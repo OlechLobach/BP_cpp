2f96d71e9eb84674c54bfc8c7786c9fbc6f8c1f7
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-02 01:00:03 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -2,5 +2,28 @@
 
 int main() {
 
-	return 0
-}
+        double number1, number2;
+
+        std::cout << "Enter the first number: ";
+        std::cin >> number1;
+
+        std::cout << "Eter the second number: ";
+        std::cin >> number2;
+
+        if (number1 == number2) {
+            std::cout << "these numbers are equal." << std::endl;
+        }
+        else {
+            std::cout << "these numbers are not equal. in ascendingorder: ";
+
+            if (number1 < number2) {
+                std::cout << number1 << ", " << number2 << std::endl;
+            }
+            else {
+                std::cout << number2 << ", " << number1 << std::endl;
+            }
+        }
+
+        return 0;
+    }
+
