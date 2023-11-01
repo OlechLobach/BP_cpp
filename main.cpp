87990b57d2dc1645aabbb49db7fd8728c7055920
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-02 00:22:14 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 
 int main() {
+        double number1, number2;
 
-	return 0
-}
+        std::cout << "Enter the first number: ";
+        std::cin >> number1;
+
+        std::cout << "ÂEnter the second number: ";
+        std::cin >> number2;
+
+        if (number1 < number2) {
+            std::cout << "smaller number: " << number1 << std::endl;
+        }
+        else {
+            std::cout << "smaller number: " << number2 << std::endl;
+        }
+
+        return 0;
+    }
+
