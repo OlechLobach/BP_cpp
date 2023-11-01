044f7e211e5efdcc7254698d6efcda1360b4995a
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-02 01:31:32 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 
 int main() {
+    int max_number;
 
-	return 0
+    std::cout << "Enter seven integers:" << std::endl;
+
+    std::cin >> max_number;
+
+    for (int i = 1; i < 7; ++i) {
+        int number;
+        std::cin >> number;
+
+        if (number > max_number) {
+            max_number = number;
+        }
+    }
+
+    std::cout << "The maximum number among the inputs is: " << max_number << std::endl;
+	return 0;
 }
