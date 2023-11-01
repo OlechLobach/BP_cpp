cb07eeeaf525179ba242a213ca70fd6016ea8a44
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-02 00:26:43 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 
 int main() {
+    double number;
 
-	return 0
+    std::cout << "Enter the number: ";
+    std::cin >> number;
+
+    if (number > 0) {
+        std::cout << "is a positive number." << std::endl;
+    }
+    else if (number < 0) {
+        std::cout << "is a negative number." << std::endl;
+    }
+    else {
+        std::cout << "this number is zero." << std::endl;
+    }
+	return 0;
 }
