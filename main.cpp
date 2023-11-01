ea197caf248686f70c61ca8635fdc9b36629b444
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-02 01:29:11 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 
 int main() {
+    int number;
 
-	return 0
+    std::cout << "Enter a four-digit number: ";
+    std::cin >> number;
+
+    if (number < 1000 || number >= 10000) {
+        std::cout << "Error! Please enter a four-digit number." << std::endl;
+    }
+    else {
+        int first_digit = number / 1000; 
+        int second_digit = (number / 100) % 10; 
+        int third_digit = (number / 10) % 10;  
+        int fourth_digit = number % 10;  
+
+        
+        int new_number = (second_digit * 1000) + (first_digit * 100) + (fourth_digit * 10) + third_digit;
+
+        std::cout << "The new number after the swap: " << new_number << std::endl;
+    }
+
+	return 0;
 }
